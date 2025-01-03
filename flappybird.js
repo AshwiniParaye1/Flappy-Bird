ce073c53dd57c72629c6ac6b7060539dddb091a7
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-03 09:07:14 +0530

    added the bird to canvas

--- flappybird.js
+++ flappybird.js
@@ -5,9 +5,30 @@ let boardWidth = 360;
 let boardHeight = 640;
 let context;
 
+//bird
+let birdWidth = 34; //width/height ration = 408/228 = 17/12
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight
+};
+
 window.onload = function () {
   board = document.getElementById("board");
   board.height = boardHeight;
   board.width = boardWidth;
-  context = getContext("2d"); //used for drawing on the board
+  context = board.getContext("2d"); //used for drawing on the board
+
+  //loading the img
+  birdImg = new Image();
+  birdImg.src = "./images/flappybird.png";
+  birdImg.onload = function () {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  };
 };
